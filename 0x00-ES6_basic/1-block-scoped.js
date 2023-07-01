ee36b54ce63508e8c717e1f1cc324b5f776ceef8
kmazengia <kefecoder@gmail.com>
2023-07-01 11:37:47 +0000

    block scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,9 +1,11 @@
-export default functiontaskBlock(trueOrFalse){
-    const task = false;
-    const task2 =true;
-    if (trueOrFalse){
-        const task = true;
-	const task2 = false;
-       }
-    return[task, task2];
+export default function taskBlock(trueOrFalse) {
+	  const task = false;
+	  const task2 = true;
+
+	  if (trueOrFalse) {
+		      var task = true;
+		      var task2 = false;
+		    }
+
+	  return [task, task2];
 }
