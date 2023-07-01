f4b12e8d3d8e6483d9619e1426bfd8037738be6b
kmazengia <kefecoder@gmail.com>
2023-07-01 20:40:19 +0000

    loop

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
 for (let value of array) {
-value = appendString + value;
-}
+	value = appendString + value;
+	}
 
 return array;
 }
