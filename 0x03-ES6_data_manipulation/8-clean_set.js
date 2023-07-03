a3b25ed30a2ab38a3e6980359c94b51b4d21b545
kmazengia <kefecoder@gmail.com>
2023-07-03 22:45:04 +0000

    clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,11 +1,11 @@
-export default function cleanSet(_set, startString) {
-  if (typeof _set !== 'object') return '';
-  if (typeof startString !== 'string') return '';
-  if (startString.length === 0) return '';
-
-  const stringSet = [];
-  [..._set].forEach((x) => {
-    if (x && x.indexOf(startString) === 0) stringSet.push(x.replace(startString, ''));
-  });
-  return stringSet.join('-');
-
+// eslint-disable-next-line consistent-return
+export default function cleanSet(set, startString) {
+  let res = '';
+  if (!startString || !startString.length) return res;
+  for (const el of set) {
+    if (el && el.startsWith(startString)) {
+      res += `${el.slice(startString.length)}-`;
+    }
+  }
+  return res.slice(0, res.length - 1);
+}
