4ecc6f18a3d601b2cf20c303cda0a5238cc5bba2
kmazengia <kefecoder@gmail.com>
2023-07-04 12:18:24 +0000

    cars

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,19 +1,41 @@
 export default class Car {
   constructor(brand, motor, color) {
-    if (typeof brand !== 'string') {
-      throw new TypeError('Brand must be a string');
-    } else if (typeof motor !== 'string') {
-      throw new TypeError('Motor must be a string');
-    } else if (typeof color !== 'string') {
-      throw new TypeError('Color must be a string');
-    }
+    this.brand = brand;
+    this.motor = motor;
+    this.color = color;
+  }
 
-    this._brand = brand;
-    this._motor = motor;
-    this._color = color;
+  get brand() {
+    return this._brand;
+  }
+
+  set brand(value) {
+    this._brand = value;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  set motor(value) {
+    this._motor = value;
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  set color(value) {
+    this._color = value;
+  }
+
+  static get [Symbol.species]() {
+    return this;
   }
 
   cloneCar() {
-    return this;
+    const Species = this.constructor[Symbol.species];
+
+    return new Species();
   }
 }
